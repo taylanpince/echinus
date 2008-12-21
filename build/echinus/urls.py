9104edde6409a0bdce53f13f3ae0bc8311e32a5e
Taylan Pince <taylanpince@gmail.com>
2008-12-21 12:54:26 +0200

    Added some default settings, a base template, media files in the django project.

--- build/echinus/urls.py
+++ build/echinus/urls.py
@@ -1,17 +1,11 @@
+from django.contrib import admin
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^echinus/', include('echinus.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    # Admin
+    (r'^admin/(.*)', admin.site.root),
 )
